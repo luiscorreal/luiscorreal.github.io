3da8345f8700119b082536f829f03463a38a3f51
luiscorreal <leduardocorreal@gmail.com>
2022-08-12 20:59:02 -0500

    7

--- MrHide/main.js
+++ MrHide/main.js
@@ -1,10 +1,17 @@
 if(typeof window['MrHide'] !== 'function'){
     window.MrHide = class  {
         static username=window.location.hostname.split('.')[0];
+        static meta=window.location.pathname.split('/')[1];
+        static file=window.location.pathname.split('/')[2];
 
-        static toString(){
-            console.log('username:',this.username);
+        static build(){
+            console.log(this);
         }
 
     }
+
+    window.addEventListener('load',function(evt){
+        MrHide.build();
+    });
+
 }
