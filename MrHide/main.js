f126a6400dc186de536e03ccefc830c74fac586b
luiscorreal <leduardocorreal@gmail.com>
2022-08-12 20:18:50 -0500

    6

--- MrHide/main.js
+++ MrHide/main.js
@@ -2,8 +2,8 @@ if(typeof window['MrHide'] !== 'function'){
     window.MrHide = class  {
         static username=window.location.hostname.split('.')[0];
 
-        toString(){
-            console.log('username:');//,this.username);
+        static toString(){
+            console.log('username:',this.username);
         }
 
     }
