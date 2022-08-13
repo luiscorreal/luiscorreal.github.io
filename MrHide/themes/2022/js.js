c6c85d8ae2faf71143d78ef0465dbb2a233a507d
luiscorreal <leduardocorreal@gmail.com>
2022-08-13 13:15:01 -0500

    60

--- MrHide/themes/2022/js.js
+++ MrHide/themes/2022/js.js
@@ -1,10 +1,7 @@
 MrHide.builders=Object.assign(MrHide.builders,{
     pagesNavbar(){
-        var ret='<nav>';
-        /*this.layouts['page'].forEach((item, i) => {
-            ret+=`<a href='${item.url}'>${item.title}</a>`;
-        });*/
-        for (const item in this.layouts['page']) {
+        var ret='<nav>',item;
+        for (item in this.layouts['page']) {
             item=this.layouts['page'][item];
             console.log(item)
             ret+=`<a href='${item.url}'>${item.title}</a>`;
