8603fe784b625aeb5843be74c9a5a6ce196d8d35
luiscorreal <leduardocorreal@gmail.com>
2022-08-12 21:41:23 -0500

    14

--- MrHide/main.js
+++ MrHide/main.js
@@ -1,17 +1,49 @@
 if(typeof window['MrHide'] !== 'function'){
     window.MrHide = class  {
-        static username=window.location.hostname.split('.')[0];
-        static meta=window.location.pathname.split('/')[1];
-        static file=window.location.pathname.split('/')[2];
+        static username;
+        static base;
+        static meta;
+        static file;
 
         static build(){
-            console.log(this);
+            var slashsplit=window.location.pathname.split('/');
+
+            this.username=window.location.hostname.split('.')[0];
+            this.base='https://'+window.location.hostname;
+            this.meta=slashsplit[1];
+            this.file=slashsplit[2];
+
+            if(slashsplit.length===2){//is page
+                this.file=this.meta;
+                this.base+='/MrHide/pages/';
+                fetch(this.base+this.file+'.html').then(data=>data.text()).then(html=>{
+                    this.html(`<h1>${this.file}</h1>`+html)
+                })
+
+            }else{//is meta type(post,...)
+                switch(this.meta){
+                    case 'post':this.post();break;
+                }
+            }
+
+
+        }
+
+        static html(contents=''){
+            document.body.innerHTML+=contents;
+        }
+
+        static page(){
+            this.html()
+        }
+
+        static post(){
+            this.html()
         }
 
     }
 
     window.addEventListener('load',function(evt){
-        console.log('Hello from this callback!')
         MrHide.build();
     });
 
