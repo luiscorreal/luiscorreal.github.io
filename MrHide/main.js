d4b5020ed67ba92af01a23be9914a49673a7f8ac
luiscorreal <leduardocorreal@gmail.com>
2022-08-12 22:46:26 -0500

    16

--- MrHide/main.js
+++ MrHide/main.js
@@ -1,44 +1,66 @@
 if(typeof window['MrHide'] !== 'function'){
     window.MrHide = class  {
         static username;
-        static base;
-        static meta;
+        static root;//url well formed
+        static type;
         static file;
+        static content='';
+        static settings;
 
         static build(){
             var slashsplit=window.location.pathname.split('/');
 
             this.username=window.location.hostname.split('.')[0];
-            this.base='https://'+window.location.hostname;
-            this.meta=slashsplit[1];
+            this.root='https://'+window.location.hostname;
+            this.type=slashsplit[1];
             this.file=slashsplit[2];
 
-            if(slashsplit.length===2){//is page
-                this.file=this.meta;
-                this.base+='/MrHide/pages/';
-                fetch(this.base+this.file+'.html').then(data=>data.text()).then(html=>{
-                    this.html(`<h1>${this.file}</h1>`+html)
-                })
+            //settings
+            fetch(this.root+'/MrHide/settings.json').then(data=>data.json()).then(data=>{
+                //set default settings
+                this.settings=Object.assign({
+                    "theme":""
+                },data);
 
-            }else{//is meta type(post,...)
-                switch(this.meta){
-                    case 'post':this.post();break;
+                if(slashsplit.length===2){//is page
+                    this.file=this.type;
+                    this.type='page';
+
+
+
+                }else{//is meta type(post,...)
+                    switch(this.type){
+                        case 'post':this.post();break;
+                    }
                 }
-            }
-
-
+            })
         }
 
-        static html(contents=''){
-            document.body.innerHTML+=contents;
+        static process(){
+            fetch(`${this.root}/MrHide/${this.type}s/${this.file}.html`).then(data=>data.text()).then(html=>{
+                this.content=html;
+
+                if(this.settings.theme===''){
+                    this.html(this.content);
+                }else{
+                    var themeUrl=this.root+'/MrHide/themes/'+this.settings.theme+'/';
+                    fetch(themeUrl+'layouts/'+this.type+'.html').then(data=>data.text()).then(data=>{
+                        this.html(data)
+                    })
+                }
+
+            })
         }
 
-        static page(){
-            this.html()
-        }
+        static html(contents){
+            var regex=/{{this::(.+)}}/g;
 
-        static post(){
-            this.html()
+            const newContents = contents.replace(regex, (match, $1) => {
+              return this[$1]
+            });
+
+            document.body.innerHTML+=newContents;
+
         }
 
     }
