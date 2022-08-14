87436cc0446b2b39141128b8326fed88ba480678
luiscorreal <leduardocorreal@gmail.com>
2022-08-14 14:25:53 -0500

    16

--- MrHide/main.js
+++ MrHide/main.js
@@ -139,7 +139,7 @@ console.log(name,MrHide)
                     this.layout='page'
                 }
 
-                console.log(MrHide.layout)
+                console.log(this.layout)
 
                 //list of public pages
                 this.layouts.add('page',this.path.pages+'list.json').then(pages=>{
