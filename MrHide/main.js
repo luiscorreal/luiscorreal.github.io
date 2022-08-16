6fa52653590f076e263486cf7ae2caba82886da3
luiscorreal <leduardocorreal@gmail.com>
2022-08-15 20:04:25 -0500

    ...

--- MrHide/main.js
+++ MrHide/main.js
@@ -110,7 +110,7 @@ if(typeof window['MrHide'] !== 'function'){
 
             setFields(obj){
                 Object.assign(this, obj)
-                console.log(MrHide.url)
+                console.log(MrHide.file)
             }
         }
 
